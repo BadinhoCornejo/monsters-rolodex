ffada49cc35a01d7508a33e55c5f80958dbbcfba
BadinhoCornejo <badinhocornejo@gmail.com>
2019-08-30 12:37:33 -0500

    Updates

---
+++ precache-manifest.365f765eb53d67d325b98ab583f0dfe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d605da08e8dd2f3c46e278eaa6fda7b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1039a772f808b9b039ab",
+    "url": "/monsters-rolodex/static/css/main.c39ba30d.chunk.css"
+  },
+  {
+    "revision": "bb9242d8af2f55563298",
+    "url": "/monsters-rolodex/static/js/2.02726050.chunk.js"
+  },
+  {
+    "revision": "1039a772f808b9b039ab",
+    "url": "/monsters-rolodex/static/js/main.a69233ba.chunk.js"
+  },
+  {
+    "revision": "37bb2cab5048c1123c5a",
+    "url": "/monsters-rolodex/static/js/runtime~main.2855428c.js"
+  }
+]);
